19947548cec8bc6fe1b970e39e9f10243c0f5354
Mezmerizxd <rorypearson2003@hotmail.com>
2023-11-03 23:32:31 +0000

    Remove unnecessary files and dependencies

--- src/client/src/components/Elements/Button/Button.test.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from './Button';
-import renderer from 'react-test-renderer';
-
-describe('Button', () => {
-  it('renders correctly', () => {
-    const tree = renderer.create(<Button />).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-});
